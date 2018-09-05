331c869b71ead4c722ea8d5a7adf75618b3188a5
Daniel <danrpts@gmail.com>
2018-09-05 15:28:52 -0700

    Camille's Challenge #2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # snake
+
+[comment]: <> (Camille's Challenge #2 (easy))
+[comment]: <> (TODO: write a README for your Snake game using Markdown)
--- index.js
+++ index.js
@@ -1,31 +1,93 @@
-const CANVAS_SIZE = 600;
-const SPRITE_SIZE = 50;
+const CANVAS_SIZE = 300;
+const SPRITE_SIZE = 15;
+const KEY_CODES = {
+  37: "la",
+  38: "ua",
+  39: "ra",
+  40: "da",
+  65: "A",
+  68: "D",
+  83: "S",
+  87: "W"
+};
 var canvas = null;
 var ctx = null;
 var snake = [randomSprite()];
 var food = randomSprite();
+
+const W_KEY_EL = document.getElementById("w");
+const A_KEY_EL = document.getElementById("a");
+const S_KEY_EL = document.getElementById("s");
+const D_KEY_EL = document.getElementById("d");
+
+document.addEventListener("keydown", function(event) {
+  let head = snake[0];
+  switch (KEY_CODES[event.which]) {
+    case "ua":
+    case "W": {
+      W_KEY_EL.classList.add("key-selected");
+      if (head.dir != "d") {
+        head.dir = "u";
+      }
+      break;
+    }
+    case "la":
+    case "A": {
+      A_KEY_EL.classList.add("key-selected");
+      if (head.dir != "r") {
+        head.dir = "l";
+      }
+      break;
+    }
+    case "da":
+    case "S": {
+      S_KEY_EL.classList.add("key-selected");
+      if (head.dir != "u") {
+        head.dir = "d";
+      }
+      break;
+    }
+    case "ra":
+    case "D": {
+      D_KEY_EL.classList.add("key-selected");
+      if (head.dir != "l") {
+        head.dir = "r";
+      }
+      break;
+    }
+  }
+});
+
+document.addEventListener("keyup", function(event) {
+  switch (KEY_CODES[event.which]) {
+    case "ua":
+    case "W": {
+      W_KEY_EL.classList.remove("key-selected");
+      break;
+    }
+    case "la":
+    case "A": {
+      A_KEY_EL.classList.remove("key-selected");
+      break;
+    }
+    case "da":
+    case "S": {
+      S_KEY_EL.classList.remove("key-selected");
+      break;
+    }
+    case "ra":
+    case "D": {
+      D_KEY_EL.classList.remove("key-selected");
+      break;
+    }
+  }
+});
 
 function initGame() {
   canvas = document.getElementById("canvas");
   canvas.width = CANVAS_SIZE;
   canvas.height = CANVAS_SIZE;
   ctx = canvas.getContext("2d");
-  document.addEventListener("keydown", function(event) {
-    switch (event.which) {
-      case 87: // w
-        snake[0].dir = "u";
-        break;
-      case 65: // a
-        snake[0].dir = "l";
-        break;
-      case 83: // s
-        snake[0].dir = "d";
-        break;
-      case 68: //d
-        snake[0].dir = "r";
-        break;
-    }
-  });
   animationLoop();
 }
 
@@ -45,25 +107,53 @@ function randomComponent() {
 }
 
 function randomSprite() {
-  return { x: randomComponent(), y: randomComponent() };
+  return {
+    x: randomComponent(),
+    y: randomComponent()
+  };
 }
 
 function isCollision(sprite1, sprite2) {
   return sprite1.x === sprite2.x && sprite1.y === sprite2.y;
 }
 
+function hasFoodCollision() {
+  return isCollision(snake[0], food);
+}
+
+function hasWallCollision() {
+  // TODO Camille's Challenge #2 (easy)
+  // detect a collision between the snake's head and any of the four walls
+  return false;
+}
+
+function hasSnakeCollision() {
+  // TODO Camille's Challenge #2 (easy)
+  // detect a collision between the snake's head and any sprite in the tail
+  return false;
+}
+
 function animationLoop() {
   canvas.width = canvas.width;
+  drawSprite(food.x, food.y);
   drawGrid();
-  drawSnake();
-  drawFood();
-  if (isCollision(snake[0], food)) {
-    food = randomSprite();
+  updateSnake();
+  if (hasFoodCollision()) {
     appendSnake();
+    // TODO Camille's Challenge #2 (medium)
+    // make sure the food does not spawn on any of the snake sprites
+    food = randomSprite();
+  }
+  drawSnake();
+  if (hasSnakeCollision() || hasWallCollision()) {
+    // todo: cancel animation loop, init new game
+    drawSprite(snake[0].x, snake[0].y, "red");
+    console.log("Game Over");
+    return;
   }
   setTimeout(function() {
     window.requestAnimationFrame(animationLoop);
-  }, 200);
+  }, 160);
 }
 
 function drawGrid() {
@@ -81,92 +171,69 @@ function drawGrid() {
 
 function drawSprite(x, y, color = "black") {
   ctx.fillStyle = color;
+  if (x < 0) {
+    x = CANVAS_SIZE - SPRITE_SIZE;
+  }
+  if (y < 0) {
+    y = CANVAS_SIZE - SPRITE_SIZE;
+  }
+  x %= CANVAS_SIZE;
+  y %= CANVAS_SIZE;
   ctx.fillRect(x, y, SPRITE_SIZE, SPRITE_SIZE);
 }
 
-function drawFood() {
-  drawSprite(food.x, food.y);
-  // TODO Camille's Challenge (easy)
-  // draw the food sprite on the canvas
+function updateSnake() {
+  for (let i = snake.length - 1; i >= 0; i--) {
+    switch (snake[i].dir) {
+      case "u":
+        snake[i].y -= SPRITE_SIZE;
+        break;
+      case "l":
+        snake[i].x -= SPRITE_SIZE;
+        break;
+      case "d":
+        snake[i].y += SPRITE_SIZE;
+        break;
+      case "r":
+        snake[i].x += SPRITE_SIZE;
+        break;
+    }
+    if (i > 0) {
+      snake[i].dir = snake[i - 1].dir;
+    }
+  }
 }
 
 function appendSnake() {
-  var tail = snake[snake.length-1];
+  var tail = snake[snake.length - 1];
   var sprite = {
     x: tail.x,
     y: tail.y,
     dir: tail.dir
-  }
+  };
   switch (sprite.dir) {
-    case 'u':
+    case "u":
       sprite.y += SPRITE_SIZE;
       // todo
       break;
-    case 'l':
+    case "l":
       sprite.x += SPRITE_SIZE;
       // todo
       break;
-    case 'd':
+    case "d":
       sprite.y -= SPRITE_SIZE;
       // todo
       break;
-    case 'r':
+    case "r":
       sprite.x -= SPRITE_SIZE;
-       // todo
+      // todo
       break;
   }
-  if (sprite.x < 0) {
-    sprite.x = CANVAS_SIZE - SPRITE_SIZE;
-  }
-  if (sprite.y < 0) {
-    sprite.y = CANVAS_SIZE - SPRITE_SIZE;
-  }
-  sprite.x %= CANVAS_SIZE;
-  sprite.y %= CANVAS_SIZE;
   snake.push(sprite);
-  // TODO Camille's Challenge (medium)
-  // append a sprite to the snake's data structure
 }
 
 function drawSnake() {
-    for(let i = snake.length-1; i >= 0; i--) {
-      switch (snake[i].dir) {
-        case 'u':
-          snake[i].y -= SPRITE_SIZE;
-          // todo
-          break;
-        case 'l':
-          snake[i].x -= SPRITE_SIZE;
-          // todo
-          break;
-        case 'd':
-         snake[i].y += SPRITE_SIZE;
-          // todo
-          break;
-        case 'r':
-          snake[i].x += SPRITE_SIZE;
-           // todo
-          break;
-      }
-      if (snake[i].x < 0) {
-        snake[i].x = CANVAS_SIZE - SPRITE_SIZE;
-      }
-      if (snake[i].y < 0) {
-        snake[i].y = CANVAS_SIZE - SPRITE_SIZE;
-      }
-      snake[i].x %= CANVAS_SIZE;
-      snake[i].y %= CANVAS_SIZE;
-      drawSprite(snake[i].x, snake[i].y, "blue");
-      ctx.fillStyle = "white";
-      ctx.font = '48px serif';
-      ctx.fillText(i, snake[i].x + SPRITE_SIZE/4, snake[i].y + SPRITE_SIZE/1.25, SPRITE_SIZE);
-      if (i > 0) {
-        snake[i].dir = snake[i-1].dir;
-      }
+  for (let i = snake.length - 1; i >= 0; i--) {
+    drawSprite(snake[i].x, snake[i].y, "blue");
   }
-
-  // TODO Camille's Challenge (hard)
-  // given the snakes data structure
-  // 1) draw the snake-sprites on the canvas, and
-  // 2) update the sprite directions for the next draw
 }
